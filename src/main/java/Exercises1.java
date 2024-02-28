54ce1eefd1b36fd632cef236de8ce0100152e178
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-02-28 19:56:38 +0330

    Update Exercises1.java

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -3,20 +3,31 @@ public class Exercises1 {
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
-
 
     /*
     implement a function that return nth number of fibonacci series
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
-    }
+        if (n <= 1) {
+            return n;
+        }
+        long fib = 1;
+        long prevFib = 1;
 
+        for (int i = 2; i < n; i++) {
+            long temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
 
     /*
     implement a function that returns a triangle for example:
@@ -30,13 +41,27 @@ public class Exercises1 {
 
      */
     public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+        char[][] triangle = new char[rows][];
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new char[i+1];
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = '*';
+            }
+        }
+        return triangle;
     }
-
 
     public static void main(String[] args) {
-        // test your code here!
-    }
+        Exercises1 exercises1 = new Exercises1();
+        System.out.println("Factorial of 5: " + exercises1.factorial(5));
+        System.out.println("Fibonacci number at position 7: " + exercises1.fibonacci(7));
 
+        char[][] triangle = exercises1.generateTriangle(5);
+        for (char[] row : triangle) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
